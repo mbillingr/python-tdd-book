309419b14beff02fe62215e7f2230db258570140
Martin Billinger <flkazemakase@gmail.com>
2021-01-04 21:56:44 +0100

    add javascript code

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,6 @@
-var initialize = function() {
+window.SuperLists = {};
+window.SuperLists.initialize = function() {
     $('input[name="text"]').on('keypress', function() {
         $('.has-error').hide();
     });
-}
+};
